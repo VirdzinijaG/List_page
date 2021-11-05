f38f9d53b0a437b3967efe4efe259502e51bf328
Virdžinija <virdzinija.cikanaviciute@gmail.com>
2021-11-05 21:46:16 +0200

    ...

--- list/src/Components/Post.jsx
+++ list/src/Components/Post.jsx
@@ -1,10 +1,19 @@
+import './app.css'
+
 function Post({ data }) {
     return (
-        <div className="post">
-            <div className="id">{data.id}</div>
-            <div className="title">{data.title}</div>
-            <div className="body">{data.body}</div>
-        </div>
+        <table className="table">
+            <tr className="head">
+                <th className="headId">ID</th>
+                <th className="headTitle">Title</th>
+                <th className="headBody">Body</th>
+            </tr>
+            <tr className="vidus">
+                <td className="id">{data.id}</td>
+                <td className="title">{data.title}</td>
+                <td className="body">{data.body}</td>
+            </tr>
+        </table>
     );
 }
 
